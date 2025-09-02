cd0969db573079ba14eaad168b58d50908d12fd8
Daniel Edema <daniel.edema@ii.co.uk>
2025-09-02 18:27:20 +0100

    New updates

--- test/AutomationexerciseSpecsfiles/autotestaddtocart2.ts
+++ test/AutomationexerciseSpecsfiles/autotestaddtocart2.ts
@@ -2,7 +2,7 @@ import addTocartTest2 from "../automationexcersicePageObject/addtocart2.ts";
 import LoginAutotest from "../automationexcersicePageObject/loginpage.ts";
 
 describe("Auto test add product to cart", async () => {
-  it("Add products in cart", async () => {
+  it.skip("Add products in cart", async () => {
     await LoginAutotest.checkHomePageIsVisible();
     await addTocartTest2.productbut.click();
     await addTocartTest2.firstProductbut.click();
@@ -16,7 +16,22 @@ describe("Auto test add product to cart", async () => {
     browser.url("https://automationexercise.com");
     await browser.pause(1000);
     await addTocartTest2.quantityInCart("6");
+
   });
+
+    it.skip("View and cart brand Products", async () => {
+      browser.url("https://automationexercise.com");
+      await addTocartTest2.productbut.click();
+      await expect(browser).toHaveTitle("Automation Exercise - All Products");
+      await addTocartTest2.viewProduct1stitem.click();
+      await addTocartTest2.writereviewtext.isDisplayed();
+      await addTocartTest2.addreviewtoProduct(
+        "daniel",
+        "mike@gmail.com",
+        "review message"
+      );
+      await addTocartTest2.submitreviewbut.click();
+    });
 });
 /*
 firstly i deleted the two initial items that were added to the cart from the first test case after it had verified all the test steps.
